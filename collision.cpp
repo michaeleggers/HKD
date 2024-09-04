0015ddf08376b272c35c1d58997e15c54913c6b5
Michael Eggers <mseggers@protonmail.ch>
2024-09-04 20:36:55 +0200

    Start to refactor the code to match the article

--- collision.cpp
+++ collision.cpp
@@ -30,44 +30,65 @@ CollisionInfo CollideUnitSphereWithPlane(glm::vec3 pos, glm::vec3 velocity, Plan
     glm::vec3 basePos = pos;
 
     // Signed distance from plane to unit sphere's center
-
     float sD = glm::dot(normal, basePos - ptOnPlane);
-    printf("sD: %f\n", sD);
-    if (sD >= -1.0f && sD <= 1.0f) { // Sphere is already inside the plane.
-        glm::vec3 hitPoint = basePos - sD * normal;
-        printf("STUCK\n");
-
-        return { true, hitPoint, (1.0f - glm::abs(sD)), normal };
-    }
+    float D = glm::abs(sD);
 
     // Project velocity along the plane normal
     float velDotNormal = glm::dot(normal, velocity);
 
-    // Calculate t0, that is the time when the unit sphere hits the
-    // front face of the plane.
-    float t0 = (1.0f - sD) / velDotNormal;
+    bool embeddedInPlane = false;
+    float t0, t1;
+    if (velDotNormal <= HKD_EPSILON) { // Sphere travelling parallel to the plane
+        // Distance from unit sphere center to plane is greater than 1 => no intersection!
+        if ( D >= 1.0f ) {
+            return {false, glm::vec3(0.0f), sD, glm::vec3(0.0f) };
+        }
+        // else: Sphere is already inside the plane.
+        glm::vec3 hitPoint = basePos - sD * normal;
+        t0 = 0.0f;
+        t1 = 1.0f;
+        embeddedInPlane = true;
+    }
+    else { // N Dot D not 0! There could be an intersection!
 
-    // As above, calculate t1, that is the time when the unit sphere
-    // hits the back face of the plane.
-    float t1 = (-1.0f - sD) / velDotNormal;
+        // Calculate t0, that is the time when the unit sphere hits the
+        // front face of the plane.
+        t0 = (1.0f - sD) / velDotNormal;
 
-    // If both t0, t1 are outside the range [0, 1], then there is no collision!
-    if ( (t0 < 0.0f || t0 > 1.0f) && (t1 < 0.0f || t1 > 1.0f) ) {
-        return {false, glm::vec3(0.0f), t0, glm::vec3(0.0f) }; // Actually could also be t1?
+        // As above, calculate t1, that is the time when the unit sphere
+        // hits the back face of the plane.
+        t1 = (-1.0f - sD) / velDotNormal;
+
+        printf("t0: %f, t1: %f\n", t0, t1);
+
+        // t0, t1 marks the intersection interval. Make sure
+        // t0 < t1 because depending on what side of the Plane
+        // the sphere is, t0 might not be the smallest value.
+        // But we need the smallest because it is the maximum we can
+        // travel the sphere along the velocity vector before a collision happens.
+        if (t0 > t1) {
+            float tmp = t0;
+            t0 = t1;
+            t1 = tmp;
+        }
+
+        if (t0 > 1.0f || t1 < 0.0f) { // No collision
+            return { false };
+        }
+
+        glm::vec3 hitPoint = basePos;
+        float t = 0.0f;
+        if (t0 <= 1.0f) {
+            hitPoint += t0 * velocity - normal;
+            t = t0;
+        }
+        else if (t1 >= -1.0f) {
+            hitPoint += t1 * velocity - normal;
+            t = t1;
+        }
     }
 
-    glm::vec3 hitPoint = basePos;
-    float t = 0.0f;
-    if (t0 <= 1.0f) {
-        hitPoint += t0 * velocity + normal;
-        t = t0;
-    }
-    else if (t1 >= -1.0f) {
-        hitPoint += t1 * velocity + normal;
-        t = t1;
-    }
-
-    return { true, hitPoint, t, normal };
+    return { true, glm::vec3(0.0f), 0.0f, normal };
 }
 
 CollisionInfo CollideEllipsoidWithTriPlane(EllipsoidCollider ec, glm::vec3 velocity, TriPlane tp)
@@ -88,6 +109,11 @@ CollisionInfo CollideEllipsoidWithTriPlane(EllipsoidCollider ec, glm::vec3 veloc
 
     CollisionInfo ci = CollideUnitSphereWithPlane(
         esBasePos, esVelocity, esPlane);
+
+    if (ci.didCollide) {
+        glm::vec3 newPos = ci.hitPoint;
+
+    }
 
     // Convert back from ellipsoid space to world space.
     ci.hitPoint = glm::inverse(ec.toESpace) * ci.hitPoint;
